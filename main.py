3b7099300380f98a9c93e48fde9b10628b35dfef
Rogério Rodrigues <rogerionrc@gmail.com>
2025-02-01 17:15:05 -0300

    Quarta versão da API

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI, status, HTTPException, Depends
 from pydantic import BaseModel
 from enum import Enum
 import logging
+from groq import Groq
+import os
+from dotenv import load_dotenv
 
-logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(asctime)s: %(message)s')
+load_dotenv()
+
+logging.basicConfig(
+    level=logging.INFO, format="[%(levelname)s] %(asctime)s: %(message)s"
+)
 logger = logging.getLogger("fastapi")
 
 
@@ -14,12 +21,13 @@ class NomeGrupo(str, Enum):
 
 API_TOKEN = 123
 
+
 def commom_verificacao_api_token(api_token: int):
     if api_token != API_TOKEN:
         raise HTTPException(status_code=401, detail="Token inválido")
 
 
-description = f"""
+description = """
     API desenvolvida durante a aula 2, contendo endpoints de exemplo e soma
     
     - /teste: retorna uma mensagem de sucesso
@@ -40,38 +48,37 @@ app = FastAPI(
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
-    dependencies=[Depends(commom_verificacao_api_token)]
+    dependencies=[Depends(commom_verificacao_api_token)],
 )
 
 
-@app.get("/teste", 
-         summary="Retorna mensagem de teste", 
-         description="Retorna uma mensagem de exemplo para testar e verificar se deu certo",
-         tags=[NomeGrupo.teste])
+@app.get(
+    "/teste",
+    summary="Retorna mensagem de teste",
+    description="Retorna uma mensagem de exemplo para testar e verificar se deu certo",
+    tags=[NomeGrupo.teste],
+)
 def hello_world():
     return {"mensagem": "Deu certo"}
-
 
 
 # Criando um endpoint para receber dois números e retornar a soma
 @app.post("/soma/{numero1}/{numero2}", tags=[NomeGrupo.operacoes])
 def soma(numero1: int, numero2: int):
-    
     logger.info(f"Requisição recebida, parâmetros numero1={numero1}, numero2={numero2}")
 
-    logger.info("Verificando se algum número é negativo")
     if numero1 < 0 or numero2 < 0:
         logger.error("Não é permitido números negativos")
         raise HTTPException(status_code=400, detail="Não é permitido números negativos")
-    
+
     total = numero1 + numero2
-    
+
     if total < 0:
         logger.error("Resultado negativo")
         raise HTTPException(status_code=400, detail="Resultado negativo")
-    
+
     logger.info(f"Requisição processada com sucesso. Resultado: {total}")
-    
+
     return {"resultado": total, "warning": "Esta versão será descontinuada em 30 dias"}
 
 
@@ -85,25 +92,30 @@ def soma_formato2(numero1: int, numero2: int):
 class Numero(BaseModel):
     numero1: int
     numero2: int
-    
+  
+
 class Resultado(BaseModel):
     resultado: int
 
-@app.post("/soma/v3", response_model=Resultado, tags=[NomeGrupo.operacoes], status_code=status.HTTP_200_OK)
+
+@app.post(
+    "/soma/v3",
+    response_model=Resultado,
+    tags=[NomeGrupo.operacoes],
+    status_code=status.HTTP_200_OK,
+)
 def soma_formato3(numero: Numero):
     total = numero.numero1 + numero.numero2 + numero.numero3
     return {"resultado": total}
 
 
-
 @app.post("/divisao/{numero1}/{numero2}", tags=[NomeGrupo.operacoes])
 def divisao(numero1: int, numero2: int):
-    
     if numero2 == 0:
         raise HTTPException(status_code=400, detail="Não é permitido divisão por zero")
-        
+
     total = numero1 / numero2
-    
+
     return {"resultado": total}
 
 
@@ -116,17 +128,56 @@ class TipoOperacao(str, Enum):
 
 @app.post("/operacao", tags=[NomeGrupo.operacoes])
 def operacao(numero: Numero, tipo: TipoOperacao):
-    
     if tipo == TipoOperacao.soma:
         total = numero.numero1 + numero.numero2
-    
+
     elif tipo == TipoOperacao.subtracao:
         total = numero.numero1 - numero.numero2
-    
+
     elif tipo == TipoOperacao.multiplicacao:
         total = numero.numero1 * numero.numero2
-    
+
     elif tipo == TipoOperacao.divisao:
         total = numero.numero1 / numero.numero2
-    
+
     return {"resultado": total}
+
+
+# Receber por parâmetro um “tema” de uma história
+# Montar um prompt para que seja gerada uma história com base no tema informado pelo usuário
+# Execute o prompt na OpenAI ou Groq
+# Retorne a resposta para o usuário
+
+
+def executar_prompt(tema: str):
+    """
+    Gera uma história em português brasileiro sobre um tema específico usando a API Groq.
+    Args:
+        tema (str): O tema sobre o qual a história será escrita.
+    Returns:
+        str: O conteúdo da história gerada pela API Groq.
+    """
+    prompt = f"Escreva uma história em pt-br sobre o {tema}"
+
+    client = Groq(
+        api_key=os.getenv("GROQ_API_KEY"),
+    )
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="deepseek-r1-distill-llama-70b",
+    )
+
+    return chat_completion.choices[0].message.content
+
+
+@app.post("/gerar_historia")
+def gerar_historia(tema: str):
+    historia = executar_prompt(tema)
+
+    return {"historia": historia}
